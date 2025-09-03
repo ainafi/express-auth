ecf2e9247476e2353f47c926f4ac2a19bd9e6a69
ainafi <razanaminoainafinaritra@gmail.com>
2025-09-03 16:47:48 +0300

    Add CORS middleware to enable cross-origin requests

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import errorMiddleware from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(errorMiddleware);
